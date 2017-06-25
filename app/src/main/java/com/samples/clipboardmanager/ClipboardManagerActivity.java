7586a866ec4fde4bb5860849fa17cd7fd2209ab3
java2019 <java_2019@mail.ru>
2017-06-25 13:22:03 +0300

    test1

--- app/src/main/java/com/samples/clipboardmanager/ClipboardManagerActivity.java
+++ app/src/main/java/com/samples/clipboardmanager/ClipboardManagerActivity.java
@@ -1,13 +1,38 @@
 package com.samples.clipboardmanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ClipboardManagerActivity extends AppCompatActivity {
+public class ClipboardManagerActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ClipboardManager manager;
+    private EditText textCopy;
+    private TextView textPaste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clipboard_manager);
+
+        textCopy = (EditText) findViewById(R.id.textCopy);
+        textPaste = (TextView) findViewById(R.id.textPaste);
+        manager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bCopy:
+                manager.setText(textCopy.getText());
+                break;
+            case R.id.bPaste:
+                textPaste.append(manager.getText() + "\n");
+                break;
+        }
     }
 }
