9fb8d39130c0f7c945a725110f0121f8e8b166af
java2019 <java_2019@mail.ru>
2017-06-25 13:07:00 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/clipboardmanager/ClipboardManagerActivity.java
@@ -0,0 +1,13 @@
+package com.samples.clipboardmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClipboardManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clipboard_manager);
+    }
+}
